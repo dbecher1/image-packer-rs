722ce9a6640776749030cad02d16f7fbd166adab
dbecher <debecher93@gmail.com>
2023-12-27 20:05:51 -0500

    Created build script to set up file environment and generate python helper script

---
+++ build.rs
@@ -0,0 +1,27 @@
+
+use std::fs;
+
+fn main() {
+    // create the folder structure
+    fs::create_dir_all("packer/images").expect("Error creating directory!");
+    fs::create_dir_all("packer/out").expect("Error creating directory!");
+
+    // python script
+    let py_name = "packer/out/generate_animation_data_template.py";
+    let py_src = "from os import listdir
+from os.path import isfile, join
+
+folder = 'packer/images/'
+out_dir = 'packer/out/'
+out_name = 'animation_data.toml'
+out = ''
+
+file_names = [f for f in listdir(folder) if isfile(join(folder, f))]
+file_names_no_ext = list(map(lambda f : f.split('.')[0], file_names))
+
+for f in file_names_no_ext:
+    out += f + ' = 0\\n'
+with open(out_dir + out_name, 'w') as f:
+    f.write(out)";
+    fs::write(py_name, py_src).expect("Error generating python script!");
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ fn main() {
 
     let mut image_packer = ImagePacker::new(None);
 
-    // image_packer.generate_animation_data_template();
-
     match image_packer.read_files() {
         Ok(_) => {
             if image_packer.get_print_output() {
